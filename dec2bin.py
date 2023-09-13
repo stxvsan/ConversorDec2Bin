459ae86c321a4e170efd1203285ed6a3b36f1c2a
stxvsan <stevenenrique35@gmail.com>
2023-09-13 19:02:57 +0200

    NuevoDEC

--- dec2bin.py
+++ dec2bin.py
@@ -5,18 +5,18 @@ def dec2bin(numero_decimal, numero_bits):
 
   while len(numero_binario) < numero_bits:
     numero_binario = "0" + numero_binario
-else:
-  numero_binario = numero_binario[3:len(numero_binario)]
-  while len(numero_binario) < numero_bits:
-    numero_binario = "1" + numero_binario
-return numero_binario
+  else:
+    numero_binario = numero_binario[3:len(numero_binario)]
+    while len(numero_binario) < numero_bits:
+      numero_binario = "1" + numero_binario
+  return numero_binario
 
 
 
 
 
 if __name__ == "__main__":
-  numero_binario = int (input("Escribe el numero en decimal que quieres convertir:  "))
+  numero_decimal = int (input("Escribe el numero en decimal que quieres convertir:  "))
   numero_bits = int (input("Cuantos bits tendra el numero binario:  "))
   numero_binario = dec2bin(numero_decimal, numero_bits)
   print("El numero " + str(numero_decimal) + " es " + numero_binario + " en binario con " + str(numero_bits) + " bits.")
